be4560ce13633ca9851db4ebdc7bfd0299d2a6f0
bubble <1065046544@qq.com>
2023-09-13 15:14:26 +0800

    clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -14,13 +14,13 @@ export default function GameCard({ game }: Props): JSX.Element {
       <Card>
         <Image src={game.background_image} />
         <CardBody>
-          <Heading fontSize='2xl'>{game.name}</Heading>
-          <HStack justifyContent='space-between'>
+          <HStack justifyContent='space-between' marginBottom={3}>
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
             />
             <CiticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize='2xl'>{game.name}</Heading>
         </CardBody>
       </Card>
     </GameCardContainer>
